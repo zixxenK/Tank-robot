3a206343cdc7d29f466f4005018367d500186ccb
zixxenK <42098139+zixxenK@users.noreply.github.com>
2026-08-06 17:53:18 -0700

    Enable IWDG watchdog for safety reset on firmware crash

--- firmware/stm32_chassis/Core/Src/main.c
+++ firmware/stm32_chassis/Core/Src/main.c
@@ -23,6 +23,7 @@
 #include "crc.h"
 #include "dma.h"
 #include "i2c.h"
+#include "iwdg.h"
 #include "tim.h"
 #include "usart.h"
 #include "usb_device.h"
@@ -122,6 +123,7 @@ int main(void)
   MX_ADC1_Init();
   MX_TIM14_Init();
   MX_TIM2_Init();
+  MX_IWDG_Init();
 
   /* Initialize interrupts */
   MX_NVIC_Init();
@@ -172,8 +174,9 @@ void SystemClock_Config(void)
   /** Initializes the RCC Oscillators according to the specified parameters
   * in the RCC_OscInitTypeDef structure.
   */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_LSI|RCC_OSCILLATORTYPE_HSE;
   RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.LSIState = RCC_LSI_ON;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
   RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
   RCC_OscInitStruct.PLL.PLLM = 25;
